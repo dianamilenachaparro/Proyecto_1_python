f6e7d72137ae944185536aab4912fbaad5e49977
Dianamilenach <chaparromaciasdianamilena@gmail.com>
2026-09-01 22:09:03 -0500

    Se añade la variable sexo a usuarios e instructores. Se añade validación de datos de edad, y las opciones como especialidad, disponibilidad. Se añade función de visualizar datos de usuarios e instructores.

--- backend/clientes.py
+++ backend/clientes.py
@@ -1,16 +1,19 @@
 
 import json
 
-FILE = "usuarios.json"
-
-
-def cargar_usuarios(FILE):
+def cargar_usuarios(file):
+    try:
+        with open(file, 'r', encoding='utf-8') as archivo:
+            return json.load(archivo)
+    except FileNotFoundError:
+        return []
     
-    # Abrir y cargar el archivo JSON
-    with open(FILE, 'r', encoding='utf-8') as archivo:
-        return json.load(archivo)
-    
-
 def guardar_usuarios(name,data):
     with open(name, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4)
+
+def documento_existe(file, documento):
+    # Cargar los datos del archivo JSON
+    datos = cargar_usuarios(file)
+    # Verificar si el documento ya existe, any devuelve True si encuentra al menos un elemento que cumpla la condición, de lo contrario devuelve False
+    return any(usuario["documento"] == documento for usuario in datos)
