d2d0985d8b20855146fcefc7a2c4bd32bf5fcc79
Nicolas121705 <santiagoarchila1@gmail.com>
2026-09-02 17:05:41 -0500

    copia

--- backend/clientes.py
+++ backend/clientes.py
@@ -1,19 +1,16 @@
 
 import json
 
-def cargar_usuarios(file):
-    try:
-        with open(file, 'r', encoding='utf-8') as archivo:
-            return json.load(archivo)
-    except FileNotFoundError:
-        return []
+FILE = "usuarios.json"
+
+
+def cargar_usuarios(FILE):
     
+    # Abrir y cargar el archivo JSON
+    with open(FILE, 'r', encoding='utf-8') as archivo:
+        return json.load(archivo)
+    
+
 def guardar_usuarios(name,data):
     with open(name, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4)
-
-def documento_existe(file, documento):
-    # Cargar los datos del archivo JSON
-    datos = cargar_usuarios(file)
-    # Verificar si el documento ya existe, any devuelve True si encuentra al menos un elemento que cumpla la condición, de lo contrario devuelve False
-    return any(usuario["documento"] == documento for usuario in datos)
